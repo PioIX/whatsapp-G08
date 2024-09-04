33f46671babb8faecd57e97ce378690acff01a33
tomasmaraval <162989282+anabone@users.noreply.github.com>
2024-09-04 08:42:58 -0300

    alo

--- my-app/src/app/useSocket/page.js
+++ my-app/src/app/useSocket/page.js
@@ -1,4 +1,5 @@
 "use client"
+import Button from "@/components/button";
 import { useSocket } from "@/hooks/useSocket";
 import { useEffect } from "react";
 
@@ -7,11 +8,25 @@ export default function UsersRanking() {
     const { socket, isConected } = useSocket();
 
     useEffect(() => {
+        //evistar errores si no hay socket
+        if(!socket) return;
+
+        socket.on('pingAll', (data) => {
+            console.log("me llego el evento ping all", data);
+        });
 
     }, [socket, isConected]);
 
+    function handleClick() {
+        socket.emit('pingAll', {message: "Hola desde weSockets! Ana y So"})
+    }
+
     return(
+        <>
         <h1>soy la ruta /ranking/users</h1>
+        <Button onClick={handleClick} text="enviar pingall"></Button>
+        </>
+        
     )
 
 }
